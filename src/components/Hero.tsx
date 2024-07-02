91e0f4c7e2b18879b015c068a8da17f396273d47
bautiliendo <juanbautistaliendo1@gmail.com>
2024-07-01 22:13:59 -0300

    Mejora estilos hero mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,19 +18,23 @@ export const Hero: React.FC = () => {
   return (
 
     <>
-      <section className="my-4 bg-gray-900 sm:py-16 lg:py-24 ">
-        <div className="px-4 mx-auto sm:px-6 lg:px-10 max-w-6xl text-white">
-          <div className="flex items-end justify-between mt-40 sm:mt-0 ">
+      <section className="my-4 bg-gray-900 sm:py-16 lg:py-24 h-[90vh] flex flex-col justify-center">
+        <div className="px-4 mx-auto sm:px-6 lg:px-10 max-w-6xl text-white h-full flex flex-col justify-center">
+
+          <div className="flex flex-col justify-between sm:mt-0 pb-4">
             <div className="flex-1 text-left">
-              <h2 className="text-2xl font-bold leading-tight sm:text-4xl lg:text-5xl text-white pt-5">RENOVARTE - Córdoba AR</h2>
-              <p className="max-w-xl my-4 text-base leading-relaxed lg:mx-0 text-white hidden sm:block">La tienda donde encontrás todos los electrodomésticos, celulares y productos informáticos para renovar tu casa</p>
-              <button  onClick={() => navigate('/empresa')} className="my-4 bg-[#00df9a] text-gray-900 hover:bg-[#1ea77b] text-sm py-2 px-6 rounded-full font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg mt-5 block 1050px:hidden">Conocer más</button>
+              <h2 className="text-2xl font-bold leading-tight sm:text-4xl lg:text-5xl text-white pt-5 mb-4">RENOVARTE - Córdoba AR</h2>
+
+              <p className="max-w-xl mb-4 text-sm sm:text-base leading-relaxed lg:mx-0 text-white">La tienda donde encontrás todos los electrodomésticos, celulares y productos informáticos para renovar tu casa</p>
+
+              <button onClick={() => navigate('/empresa')} className="mb-4 bg-[#00df9a] text-gray-900 hover:bg-[#1ea77b] text-sm py-2 px-6 rounded-full font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg sm:hidden">Conocer más</button>
+
               <div>
-                <p className='md:text-3xl sm:text-2xl text-xl font-bold'>
+                <p className='text-lg sm:text-2xl md:text-3xl font-bold mb-2'>
                   Beneficios exclusivos y convenios con mutuales
                 </p>
                 <ReactTyped
-                  className='md:text-3xl sm:text-2xl text-xl font-bold text-[#00df9a]'
+                  className='text-lg sm:text-2xl md:text-3xl font-bold text-[#00df9a]'
                   strings={['SUOEM', '3 ABRIL', 'UPCN']}
                   typeSpeed={70}
                   backSpeed={50}
